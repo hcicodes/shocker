a113f80757f56c71f7d3c8b443dec8b436e1c3a4
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-10-14 16:07:34 -0400

    first

---
+++ src/main/java/com/shocker/App.java
@@ -0,0 +1,13 @@
+package com.shocker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
