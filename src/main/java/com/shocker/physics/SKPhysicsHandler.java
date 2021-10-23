0bd9f2c6a6ebd69939cfa18bf05c8843b8c5d917
JT <bailey.jt.brown@gmail.com>
2021-10-23 13:06:05 -0400

    Cleaned up PGroup related functions
    Subject to lots of changes + optimizations

--- src/main/java/com/shocker/physics/SKPhysicsHandler.java
+++ src/main/java/com/shocker/physics/SKPhysicsHandler.java
@@ -34,13 +34,32 @@
      public SKFVector[] vecBuffer    = new SKFVector[MAXPOBJS];
      public SKPhysicsGroup[] pGroups = new SKPhysicsGroup[MAXPGROUPS];
 
+     /* pgroup bound threshold */
+     public static final int PGROUPTHRESHOLDX = 500;
+     public static final int PGROUPTHRESHOLDY = 500;
+
      /* internal physics group class */
     private final class SKPhysicsGroup
     {
-       public int gX = 0; /* group X */
-       public int gY = 0; /* group Y */
-       /* array of pObjBuffer indexes */
-       public int[] pBuffIndexes = new int[MAXPGSIZE];
+        public int gX = 0; /* group X */
+        public int gY = 0; /* group Y */
+        /* array of pObjBuffer indexes */
+        int piSize = 0;
+        public int[] pBuffIndexes = new int[MAXPGSIZE];
+
+        /* ctor */
+        public SKPhysicsGroup(int gx, int gy)
+        {
+            gX = gx;
+            gY = gy;
+        }
+
+        /* add index */
+        public void addIndex(int indx)
+        {
+            pBuffIndexes[piSize] = indx;
+            piSize++;
+        }
     }
 
     /* ********************************************************
@@ -127,7 +146,73 @@
             if(pObjBuffer[i] != null)
             {
                 SKEntity scanPObj = pObjBuffer[i]; /* actively scanned PObj */
+
+                /* get pGroup number */
+                int pNumX = (int)scanPObj.position.x / PGROUPTHRESHOLDX;
+                int pNumY = (int)scanPObj.position.y / PGROUPTHRESHOLDY;
+
+                boolean foundGroup = false;
+
+                /* for every PGroup */
+                for(int j = 0; j < MAXPGROUPS; j++)
+                {
+                    /* if pGroup is active */
+                    /* and pNums are matching */
+                    if(pGroups[j] != null &&
+                       pGroups[j].gX == pNumX &&
+                       pGroups[j].gY == pNumY)
+                    {
+                        /* add and break for loop */
+                        pGroups[i].addIndex(i);
+                        foundGroup = true;
+                        break;
+                    }
+                }
+
+                /* when exit loop, check if group found */
+                /* if not, create group */
+                if(!foundGroup)
+                {
+                    /* loop thru pGroups to find empty, create and add */
+                    for(int j = 0; j < MAXPGROUPS; j++)
+                    {
+                        if(pGroups[j] == null)
+                        {
+                            /* create group, add and break */
+                            pGroups[j] = new SKPhysicsGroup(pNumX, pNumY);
+                            pGroups[j].addIndex(i);
+                            break;
+                        }
+                    }
+                } /* END PGROUP CREATION LOOP */
+            } /* END POBJ NULL CHECK */
+        } /* END POBJ CHECK LOOP */
+
+        /* pgroups are now all sorted */
+        /* if debug, log all pgroup values */
+        if(debugMode)
+        {
+            System.out.printf("----------------------------------------\n");
+            System.out.printf("LOGGIN ALL PGROUP VALS:\n");
+
+            /* loop all pgroups, log if !null */
+            for(int i = 0; i < MAXPGROUPS; i++)
+            {
+                if(pGroups[i] != null)
+                {
+                    System.out.printf("PGROUP[%d] vals:\n", i);
+                    System.out.printf("\tGroup Val: (%d, %d)\n", pGroups[i].gX, pGroups[i].gY);
+                    System.out.printf("IList: [ ");
+                    for(int j = 0; j < pGroups[i].piSize; j++)
+                    {
+                        System.out.printf("%d,", pGroups[i].pBuffIndexes[j]);
+                    }
+                    System.out.printf("]\n");
+                }
             }
+
+            /* log segment end */
+            System.out.printf("----------------------------------------\n");
         }
     }
 
